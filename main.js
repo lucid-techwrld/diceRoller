87d15412b189f3b21d6a0a33318a87969c8d5df5
lucid-techwrld <davisredtiger@gmail.com>
2024-08-25 14:32:24 +0100

    update

--- main.js
+++ main.js
@@ -3,10 +3,15 @@ function rollDice() {
   const result = document.getElementById('result');
   const numOfDice = document.getElementById('numOfDice').value;
   const totalDiceElement = document.getElementById('totalDice');
+  const error = document.getElementById('errorDisplay');
   
   const randomDices = [];
   const images = [];
+  const max = 6;
   
+  if (numOfDice > 0 && numOfDice <= max) {
+      
+      error.textContent = 'Maximum Dice to roll is 6, I wish you Luck🍀!!';
   for (let i = 0; i < numOfDice; i++) {
     const randomDice = Math.floor(Math.random() * 6) + 1;
     randomDices.push(randomDice);
@@ -18,4 +23,13 @@ function rollDice() {
   
   const total = randomDices.reduce((accumulator, element) => accumulator + element, 0);
   totalDiceElement.textContent = `Total: ${total}`;
+  }
+  else {
+    error.textContent = 'Numbuer of Dice to roll should be between 1 and 6';
+    error.style.color = 'red';
+    
+    result.textContent = '';
+    image.innerHTML = '';
+    totalDiceElement.textContent = '';
+  }
 }
